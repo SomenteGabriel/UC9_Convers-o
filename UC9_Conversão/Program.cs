425771da9941715c03ca7b10df90c88cce2b5cdd
GABRIEL VIANNA CARVALHO DOS SANTOS <gabriel.vcsantos1@senacsp.edu.br>
2023-03-07 20:08:21 -0300

    add switch

--- UC9_Conversão/Program.cs
+++ UC9_Conversão/Program.cs
@@ -1,12 +1,37 @@
 ﻿double temperatura = 0;
 double conversao = 0;
+string escolha = "";
 
-Console.WriteLine("Digite a temperatura em celsius");
-temperatura = Double.Parse(Console.ReadLine());
+Console.WriteLine("Deseja fazer a conversao para celsius ou fahrenheit? 'C' || 'F'. ");
+escolha = Console.ReadLine();
 
+switch(escolha)
 
-conversao = ( temperatura * (9/5)) + 32;
+{
 
-Console.WriteLine("");
-Console.WriteLine("");
-Console.WriteLine(conversao+", em fahrenheit");
+    case "F":
+        Console.WriteLine("Digite a temperatura em celsius")
+
+;
+        temperatura = Double.Parse(Console.ReadLine());
+
+        conversao = (( temperatura * 9/5) + 32);
+
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine(conversao+", em fahrenheit");
+                break;
+
+        case "C":
+       
+        Console.WriteLine("Digite a temperatura em Fahrenheit");
+        temperatura = Double.Parse(Console.ReadLine());
+
+        conversao = ((temperatura-32)*5/9);
+
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine(conversao.ToString() + ", em Celsius");
+
+        break;
+}
