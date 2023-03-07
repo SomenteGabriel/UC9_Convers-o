339bd717e4f83214f93a28b2a1f82e3e0c945b01
gabriel.vcsantos1 <gabriel.vcsantos1@GUA0654085W10-1>
2023-03-06 22:12:03 -0300

    Adicionar arquivos de projeto.

---
+++ UC9_Conversão/Program.cs
@@ -0,0 +1,12 @@
+﻿double temperatura = 0;
+double conversao = 0;
+
+Console.WriteLine("Digite a temperatura em celsius");
+temperatura = Double.Parse(Console.ReadLine());
+
+
+conversao = ( temperatura * (9/5)) + 32;
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine(conversao+", em fahrenheit");
